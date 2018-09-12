1f2b2028ab8d52e982f3eb3f820251b1deea3a44
catbiscuit <1606227397@qq.com>
2018-09-12 23:00:28 +0800

    201809122300

---
+++ MT/MT.Business/MT.Business.BLL/SystemManage/T_LogBLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.Business.BLL.SystemManage
+{
+    class T_LogBLL
+    {
+    }
+}
